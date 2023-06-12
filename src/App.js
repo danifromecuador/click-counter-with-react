c584f76aa7481237a16dc56ec37541e19d852ed4
dani <danifromecuador@gmail.com>
2023-06-12 12:55:59 -0500

    Add linters

--- src/App.js
+++ src/App.js
@@ -11,7 +11,11 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
       </div>
     );
