7de4c928096ee778b93ec11dfe0708ac42e41cc9
dani <danifromecuador@gmail.com>
2023-06-12 13:05:17 -0500

    Change function declaration to arrow function

--- src/App.js
+++ src/App.js
@@ -2,22 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit
-        {' '}
-        <code>src/App.js</code>
-        {' '}
-        and save to reload.
-      </p>
+const App = () => (
+  <div className="App">
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>Welcome to React</h2>
     </div>
-  );
-}
+    <p className="App-intro">
+      To get started, edit
+      {' '}
+      <code>src/App.js</code>
+      {' '}
+      and save to reload.
+    </p>
+  </div>
+);
 
 export default App;
