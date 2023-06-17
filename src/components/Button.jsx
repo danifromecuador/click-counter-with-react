f06d863ba296655f3ac6e6eef6e287862715506e
dani <danifromecuador@gmail.com>
2023-06-16 19:19:26 -0500

    Avoid negative count

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/Button.css';
 
-const Button = ({ symbol, add, subtract, restore }) => {
-
-  return (
-    <div className="button" onClick={symbol === '+' ? add : symbol==='-' ? subtract : restore } >
-      {symbol}
-    </div>
-  );
-}
+const Button = ({
+  symbol, add, subtract, restore,
+}) => (
+  <button className="button" type="button" onClick={symbol === '+' ? add : symbol === '-' ? subtract : restore}>
+    {symbol}
+  </button>
+);
 
 Button.propTypes = {
   symbol: PropTypes.string.isRequired,
+  add: PropTypes.func.isRequired,
+  subtract: PropTypes.func.isRequired,
+  restore: PropTypes.func.isRequired,
 };
 
 export default Button;
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Button from './Button';
 import '../styles/Buttons.css';
 
-const Buttons = ({add, subtract, restore}) => {  
-  return (
-    <div className="buttons">
-      <Button symbol="-" subtract={subtract} />
-      <Button symbol="&#x27F3;" restore={restore} />
-      <Button symbol="+" add={add}/>
-    </div>);
+const Buttons = ({ add, subtract, restore }) => (
+  <div className="buttons">
+    <Button symbol="-" subtract={subtract} />
+    <Button symbol="&#x27F3;" restore={restore} />
+    <Button symbol="+" add={add} />
+  </div>
+);
+
+Buttons.propTypes = {
+  add: PropTypes.func.isRequired,
+  subtract: PropTypes.func.isRequired,
+  restore: PropTypes.func.isRequired,
 };
 
 export default Buttons;
