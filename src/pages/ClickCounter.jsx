b73491dfeef052fb5b848f8aa39d21fcda3651a4
dani <danifromecuador@gmail.com>
2023-06-20 16:54:30 -0500

    Save count value on localStorage

--- src/pages/ClickCounter.jsx
+++ src/pages/ClickCounter.jsx
@@ -3,18 +3,25 @@ import Display from '../components/Display';
 import Buttons from '../components/Buttons';
 
 const ClickCounter = () => {
-  const [count, setCount] = useState(0);
+  const savedCount = parseInt(localStorage.getItem('saveCount')) || 0;  
+  const [count, setCount] = useState(savedCount);
 
-  const add = () => setCount(count + 1);
+  const add = () => {
+    setCount(count + 1);
+    localStorage.setItem('saveCount', count + 1);
+  }
 
   const subtract = () => {
     setCount(count - 1);
     if (count === 0) setCount(0);
-    console.log(count);
+    localStorage.setItem('saveCount', count - 1);
     return count;
   };
 
-  const restore = () => setCount(0);
+  const restore = () => {
+    setCount(0);
+    localStorage.setItem('saveCount', 0);
+  }
 
   return (
     <div>
